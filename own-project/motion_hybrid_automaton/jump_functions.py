a0c76da5fb3fe444b4647205af1cf63f54843895
Fabian0597 <fabian.kolb1@gmx.de>
2021-07-05 16:22:48 +0200

    refactor jump function

--- own-project/motion_hybrid_automaton/jump_functions.py
+++ own-project/motion_hybrid_automaton/jump_functions.py
@@ -10,65 +10,73 @@ from motion_hybrid_automaton.continuous_state import ContinuousState
 class JumpFunctions:
 
     def __init__(self, motion_hybrid_automaton):
-        self.model = motion_hybrid_automaton.model
-        self.slip_model = motion_hybrid_automaton.slip_model
-        self.motion_hybrid_automaton = motion_hybrid_automaton
+        self.model = motion_hybrid_automaton.model # lua leg model
+        self.slip_model = motion_hybrid_automaton.slip_model # leg model parameter
+        self.motion_hybrid_automaton = motion_hybrid_automaton # state machine
 
-    def _calculate_new_slip_length(self, time, state):
+    def _calculate_new_slip_length(self, state):
         # TODO check the Force is towards ground at the beginning of stance phase
         # TODO remove unused lines
+        """
+        modified  SLIP model with impact compensation which differs from the regular SLIP model only in the fact that the leg spring is being pre-compressed at touch-down. The precompressed leg length is calculated here
+        :param state: robot state
+        """
+        # get jacobians, robot mass, inverse of mass matrix from the function defined in the ContinuousState Class
         jac_com = state.jac_com()
         robot_mass = state.robot_mass()
-        jac_s = np.zeros((3, self.model.q_size))
-        rbdl.CalcPointJacobian(self.model, state.q, self.model.GetBodyId("foot"), np.zeros(3), jac_s, False)
-        jac_s = np.delete(jac_s, 2, 0)
-        # mass matrix
-        mass_matrix = np.zeros((self.model.q_size, self.model.q_size))
-        rbdl.CompositeRigidBodyAlgorithm(self.model, state.q, mass_matrix, False)
-        mass_matrix_inv = np.linalg.inv(mass_matrix)
+        jac_s = state.jac_s()
+        mass_matrix_inv = state.inv_mass_matrix()
+
+        # get energy loss during impact in Hutter paper (17)
         lambda_s = np.linalg.inv(jac_s @ mass_matrix_inv @ jac_s.T)
         null_s = np.eye(4) - mass_matrix_inv @ jac_s.T @ lambda_s @ jac_s
         matrix = jac_com.T @ jac_com - null_s.T @ jac_com.T @ jac_com @ null_s
+        delta_E = 0.5 * robot_mass * state.qd.T @ matrix @ state.qd
 
-        # post_vel = np.linalg.norm(jac_com @ null_s @ np.linalg.pinv(jac_com) @ state.vel_com(), ord=2)
-        delta_leg_length = (1 / self.slip_model.slip_stiffness) * robot_mass * (
-                state.qd.T @ matrix @ state.qd)
+        # get compression of spring to compensate the energy loss in Hutter paper (16)
+        delta_leg_length = (2 / self.slip_model.slip_stiffness) * delta_E
         delta_leg_length = math.sqrt(abs(delta_leg_length))
 
+        # kinematic leg length at touchdown which is used as l0 leg length during impact compensation in Hutter paper block after (17)
         foot_id = self.model.GetBodyId('foot')
         foot_pos = rbdl.CalcBodyToBaseCoordinates(self.model, state.q, foot_id, np.zeros(3), True)
         com_pos = state.pos_com()
         l0_before_impact = np.sum(com_pos - foot_pos)
 
+        # switch to a model with new leg length which compensates the impact in Hutter paper block after (17)
         self.slip_model.slip_length = l0_before_impact + delta_leg_length
-        #self.slip_model.slip_length = self.slip_model.slip_length_zero + delta_leg_length
-        # energy = 0.5 * robot_mass * state.qd.T @ matrix @ state.qd
+
 
     def flight_to_stance_jump_function(self, time, x):
-        # Calculation of change of velocity through impact
-        # qd_minus is the generalized velocity before and qd_plus the generalized velocity after the impact
         """
-        q_minus = x[:self.model.dof_count]  # first half of state vector is q
-        qd_minus = x[self.model.dof_count:]  # second half of state vector is qd
-        qd_plus = np.ones(self.model.dof_count)
-        rbdl.ComputeConstraintImpulsesDirect(self.model, q_minus, qd_minus, self.constraint, qd_plus)
-        # replace generalized velocity in state vector for next iteration by calculated velocity after ground impact
-        x[self.model.dof_count:] = qd_plus
+        Calculation of change of velocity through imp
+        :param time: current time
+        :param x: generalized velocity and vector before the impact
+        :return: generalized velocity and vector after the impact and current time
         """
+
         logging.info("change phase: flight > stance (time: %s)" % time)
 
-        # take robot snapshot
+        # used object of ContinuousState to better handle the robot state
         state = ContinuousState(self.model, x)
-        # compute impulses
-        self._calculate_new_slip_length(time, state)
-        # vel = np.linalg.norm(state.com_vel(), ord=2)
+        # compute pre-compressed leg length in order to compensate impact according to Hutter paper (16), (17)
+        self._calculate_new_slip_length(state)
+
+        # calculate the velocity after impact qd_plus
         qd_plus = np.ones(self.model.dof_count)
         rbdl.ComputeConstraintImpulsesDirect(self.model, x[:self.model.dof_count], x[self.model.dof_count:],
                                              self.motion_hybrid_automaton.current_constraint(),
                                              qd_plus)
+        # update general velocity in robot state space x = [q, qd]
         x[self.model.dof_count:] = qd_plus
         return time, x
 
     def stance_to_flight_jump_function(self, time, x):
+        """
+        Nothing is calculated while transition form stance to flight phase
+        :param time: current time
+        :param x: generalized velocity and vector
+        :return: generalized velocity and vector and current time
+        """
         logging.info("change phase: stance > flight (time: %s)" % time)
         return time, x
