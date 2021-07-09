16b7db1b43ea3b3e60ac429b23cc77f8041dc218
Stefan Schneyer <stefan.schneyer@tum.de>
2021-07-09 18:42:14 +0200

    feat(slip_model_params): Add some comments on parameters

--- own-project/model/slip_model_params.py
+++ own-project/model/slip_model_params.py
@@ -7,8 +7,8 @@ class SlipModelParameterization:
     """
 
     def __init__(self):
-        # for slip model
+        # parameter for slip model
         self.slip_length_zero = 0.9  # controls the desired leg length for the impact
-        self.slip_length = None
-        self.slip_stiffness = 10000.
+        self.slip_length = None  # actual length (is computed in jump function (flight to stance)
+        self.slip_stiffness = 10000.  # stiffness of the spring of the slip model
         self.slip_force = np.zeros(3)  # slip force #(used)
